67665a205ec123b884450ddd4120f8f0e155551c
Deepak Kanojiya <deepak6394743349@gmail.com>
2025-09-20 14:03:47 +0000

    Footer me jo text hai unko hata do

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,6 @@ const Footer = () => {
   return (
     <footer className="p-4 bg-gray-100 border-t border-gray-200">
       <div className="container mx-auto text-center text-sm text-gray-500">
-        <p>&copy; {new Date().getFullYear()} Card Manager. All rights reserved.</p>
       </div>
     </footer>
   );
