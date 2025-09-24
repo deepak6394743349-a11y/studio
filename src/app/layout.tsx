9e56e15cea18b9639b946d09b8ef7aefbbf873e5
Deepak Kanojiya <deepak6394743349@gmail.com>
2025-09-24 12:34:36 +0000

    App me zoom nahi hona chahiye

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,12 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Card Manager",
   description: "A simple app to manage your credit cards",
+  viewport: {
+    width: "device-width",
+    initialScale: 1,
+    maximumScale: 1,
+    userScalable: false,
+  },
 };
 
 export default function RootLayout({
