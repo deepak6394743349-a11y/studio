f6c47a5cd93a590105d81f68d213dab38a94d4b8
Deepak Kanojiya <deepak6394743349@gmail.com>
2025-09-24 11:59:36 +0000

    Aur niche karo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header className="p-4 bg-gradient-to-r from-gray-900 to-gray-800 text-white shadow-lg">
       <div className="container mx-auto flex items-center space-x-3">
-        <CreditCard className="w-8 h-8 mt-2" />
+        <CreditCard className="w-8 h-8 mt-8" />
       </div>
     </header>
   );
